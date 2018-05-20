f93c394b9e95586cf4fe70c6b9e96830ea3d6e5a
Ezekiel Williams <broncoscorpio@gmail.com>
2018-05-20 14:54:51 -0600

    Add entered string on screen and fix html bug.

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 var entered = '';
 var entry = '';
+var pressed = '';
+var currentNumber, log;
 
 $(document).ready(function() {
+
+    currentNumber = $("#currentNumber");
+    log = $("#entered");
+
     $('button').click(function(event) {
-        console.log($(this).html());
-        entry = $(this).html();
+        //console.log($(this).html());
+        pressed = $(this).html();
 
         //every time a button is clicked, we need to check its validity to what's already been clicked
         validate();
             //entered will update once entry is validated
             //if an operator was entered, the entry should reset
 
-        //if the entry
-        entered += entry;
+        console.log(entered);
+        console.log(entry);
 
-        $("#screen").html(entered);
+        currentNumber.html(entry);
+
+        log.html(entered);
 
         
 
@@ -23,9 +31,15 @@ $(document).ready(function() {
 });
 
 function validate() {
+    entry += pressed;
     //if last entry (last character in entered) is number && the length of number string is below limit, it's valid
-    if (entry == '+' || entry == '-' || entry == 'x') {
-        entered = '';
+    if (pressed == '+' || pressed == '-' || pressed == 'x' || pressed == '=') {
+        entered += entry;
+        entry = '';
+    }
+
+    if (pressed == '.') {
+        //check if decimal exists already or if it's the first character in the current number (a fraction)
     }
 }
 
