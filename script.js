b88e7942be07c355fd523cff19eef8b253ee0dce
unknown <broncoscorpio@gmail.com>
2018-05-26 12:33:21 -0600

    Add decimal and multiple symbol pressing handling.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var entered = '';
 var entry = '';
 var pressed = '';
+var divide = '';
 var currentNumber, log;
 
 $(document).ready(function() {
@@ -10,15 +11,15 @@ $(document).ready(function() {
 
     $('button').click(function(event) {
         //console.log($(this).html());
-        pressed = $(this).html();
+        pressed = $(this).text();
+
+        console.log(pressed);
 
         //every time a button is clicked, we need to check its validity to what's already been clicked
         validate();
             //entered will update once entry is validated
             //if an operator was entered, the entry should reset
 
-        console.log(entered);
-        console.log(entry);
 
         currentNumber.html(entry);
 
@@ -33,13 +34,29 @@ $(document).ready(function() {
 function validate() {
     entry += pressed;
     //if last entry (last character in entered) is number && the length of number string is below limit, it's valid
-    if (pressed == '+' || pressed == '-' || pressed == 'x' || pressed == '=') {
-        entered += entry;
-        entry = '';
+    if (pressed == '+' || pressed == '-' || pressed == 'x' || pressed == '=' || pressed == '/') {
+
+        if (pressed == '=') {
+            //handle evaluation
+        }
+        else {
+            //if the only button pressed is a symbol, the symbol shouldn't be logged into what was entered, so wipe pressed
+            if (entry.length == 1) {
+                entry = '';
+            }
+
+            entered += entry;
+            entry = '';
+        }
     }
 
     if (pressed == '.') {
         //check if decimal exists already or if it's the first character in the current number (a fraction)
+        //if the entry string without the last decimal pressed still has a decimal, slice it off
+        if (entry.slice(0, -1).includes('.')) {
+            entry = entry.slice(0, -1);
+        }
     }
+
 }
 
