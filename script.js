688f7d5c3f806b67c2368be5b0a7fe6ff618e3b9
unknown <broncoscorpio@gmail.com>
2018-05-27 11:10:48 -0600

    Add AC and CE buttons and functions. Decimal fixes

--- script.js
+++ script.js
@@ -10,22 +10,38 @@ $(document).ready(function() {
     log = $("#entered");
 
     $('button').click(function(event) {
+
+        var cleared = false;
         //console.log($(this).html());
         pressed = $(this).text();
 
         console.log(pressed);
 
+
+        if (pressed == 'AC' || pressed == 'CE') {
+            cleared = true;
+
+            if (pressed == 'AC') {
+                entry = '';
+                entered = '';
+            }
+    
+            if (pressed == 'CE') {
+                entry = '';
+            }
+        }
+
+        if (cleared) {
+            currentNumber.html('0');
+        }
+        else {
+            validate();
+            currentNumber.html(entry);
+        }
         //every time a button is clicked, we need to check its validity to what's already been clicked
-        validate();
-            //entered will update once entry is validated
-            //if an operator was entered, the entry should reset
-
-
-        currentNumber.html(entry);
+        
 
         log.html(entered);
-
-        
 
     });
 
@@ -36,21 +52,24 @@ function validate() {
     //if last entry (last character in entered) is number && the length of number string is below limit, it's valid
     if (pressed == '+' || pressed == '-' || pressed == 'x' || pressed == '=' || pressed == '/') {
 
-        if (pressed == '=') {
-            //handle evaluation
-        }
-        else {
             //if the only button pressed is a symbol, the symbol shouldn't be logged into what was entered, so wipe pressed
-            if (entry.length == 1) {
-                entry = '';
-            }
-
-            entered += entry;
+        if (entry.length == 1) {
             entry = '';
+        }
+
+        entered += entry;
+        entry = '';
+
+        if (pressed == '=') {
+
         }
     }
 
     if (pressed == '.') {
+        //adds leading zero
+        if (entry.length == 1) {
+            entry = '0.';
+        }
         //check if decimal exists already or if it's the first character in the current number (a fraction)
         //if the entry string without the last decimal pressed still has a decimal, slice it off
         if (entry.slice(0, -1).includes('.')) {
