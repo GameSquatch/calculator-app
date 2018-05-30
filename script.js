3f7a3e4444daf4c358b9a623be9458aac3e427f2
unknown <broncoscorpio@gmail.com>
2018-05-30 13:55:26 -0600

    Change EVERYTHING, but it works much better.

--- script.js
+++ script.js
@@ -1,137 +1,126 @@
 var entered = '';
 var entry = '';
 var pressed = '';
-var divide = '';
-var numbers = [];
 var currentNumber, log;
+var nums = [];
+var ops = [];
+
 
 $(document).ready(function() {
 
-    currentNumber = $("#currentNumber");
+    currentNumber = $("#currentNumber");//consider changing to currentEntry
     log = $("#entered");
 
     $('button').click(function(event) {
 
-        var cleared = false;
-        //console.log($(this).html());
         pressed = $(this).text();
 
-        //console.log(pressed);
-        entry += pressed;
-
         if (pressed == 'AC' || pressed == 'CE') {
-            cleared = true;
-            
-
-            if (pressed == 'AC') {
-                entry = '';
-                entered = '';
-                numbers = [];
-            }
-    
-            if (pressed == 'CE') {
-                entry = '';
-            }
-        }
-
-        if (cleared) {
-            currentNumber.html('0');
+            clear();
         }
         else {
-            validate();
-            currentNumber.html(entry);
+            var numCheck = parseInt(pressed);
+
+            if (!isNaN(numCheck) || pressed == '.') {
+                //continue entry
+                checkPressed();
+            }
+            else {
+                if (pressed == '=') {
+                    calc();
+                }
+                else {
+                    //log entry number and operation
+                    logOperation();
+                }
+            }
         }
-        //every time a button is clicked, we need to check its validity to what's already been clicked
-        
 
-        log.html(entered);
+    });//END button.click
 
-    });
+});//END doc.ready
 
-});
-
-function validate() {
-    var last = entered.charAt(entered.length - 1);
-    if (last == '=' && entry != '') {
-        entry = '';
-        entered = '';
+function clear() {
+    if (pressed == 'AC') {
+        nums = [];
+        ops = [];
+        currentNumber.html('0');
+        log.html('0');
     }
-
-    //if last entry (last character in entered) is number && the length of number string is below limit, it's valid
-    if (pressed == '+' || pressed == '-' || pressed == 'x' || pressed == '/') {
-
-        //if the only button pressed is a symbol, the symbol shouldn't be logged into what was entered, so wipe entry
-        if (entry.length == 1) {
-            entry = '';
-        }
-
-        //TO-DO add functionality for negative numbers
-
-        entered += entry;
-        var obj = {};
-        obj[pressed] = parseFloat(entry);
-        numbers.push(obj);
-        entry = '';
-
+    else {
+        currentNumber.html('0');
     }
+}
 
-    if (pressed == '=') {
-        //var last = entered.charAt(entered.length - 1);
-        var elast = entry.charAt(entry.length - 2);
-        console.log(elast);
-        if (entered != '' && entry != '' && elast != '.') {
-            entered += entry;
-            var obj = {};
-            obj[pressed] = parseFloat(entry);
-            numbers.push(obj);
-            calc();
-        }
-        else {
-            entry = entry.slice(0, -1);
-        }
-        
-    }
-
+function checkPressed() {
     if (pressed == '.') {
-        //adds leading zero
-        if (entry.length == 1) {
-            entry = '0.';
+        if (entry == '' || entry == '0') {
+            entry = '0' + pressed;
         }
-        //check if decimal exists already or if it's the first character in the current number (a fraction)
-        //if the entry string without the last decimal pressed still has a decimal, slice it off
-        if (entry.slice(0, -1).includes('.')) {
-            entry = entry.slice(0, -1);
+        else if (!entry.includes('.')) {
+            entry += '.';
         }
     }
+    else if (pressed == '0') {
+        if (entry != '' || entry != '0') {
+            entry += '0';
+        }
+    }
+    else {
+        entry += pressed;
+    }
 
+    currentNumber.html(entry);
+}
+
+function logOperation() {
+    //check that operations aren't being pressed more than once in succession
+    if (entry != '' && entry != '0') {
+        nums.push(parseFloat(entry));
+        ops.push(pressed);
+        entered += entry + pressed;
+        entry = '';
+
+        currentNumber.html(entry);
+        log.html(entered);
+    }
+
+    
 }
 
 function calc() {
-    var currentObj, nextKey;
-    var result = numbers[0][Object.keys(numbers[0])[0]];
-    
-    for (var i = 0; i < numbers.length - 1; ++i) {
 
-        currentObj = numbers[i];
-        nextKey = Object.keys(numbers[i + 1]);
-        console.log(result);
+    if (entered.length >= 2 && entry != '' && entry != '0') {
 
-        if (currentObj.hasOwnProperty('+')) {
-            result += numbers[i + 1][nextKey[0]];
+        nums.push(parseFloat(entry));
+        entered += entry + pressed;
+        log.html(entered);
+
+        var firstNum = nums[0];
+
+        for (var i = 0; i < ops.length; ++i) {
+            switch (ops[i]) {
+            case '+':
+                firstNum += nums[i + 1];
+                break;
+            case '-':
+                firstNum -= nums[i + 1];
+                break;
+            case 'x':
+                firstNum *= nums[i + 1];
+                break;
+            case '/':
+                firstNum /= nums[i + 1];
+                break;
+            }
         }
-        else if (currentObj.hasOwnProperty('-')) {
-            result -= numbers[i + 1][nextKey[0]];
-        }
-        else if (currentObj.hasOwnProperty('x')) {
-            result *= numbers[i + 1][nextKey[0]];
-        }
-        else if (currentObj.hasOwnProperty('/')) {
-            result /= numbers[i + 1][nextKey[0]];
-        }
+
+        nums = [];
+        ops = [];
+        entry = '';
+        entered = '';
+        currentNumber.html(firstNum);
+
     }
-    
-    numbers = [];
-    entry = result;
-
 }
 
